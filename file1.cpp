3d483660d6cf2e8b6ac9514138b09478954cc84d
Ibrahim-Salman27 <bscs24145@itu.edu.pk>
2024-10-10 09:55:40 +0500

    cpp file

---
+++ file1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a , b;
+    int sum = 0;
+    cout<<"Enter a: ";
+    cin>>a;
+    cout<<"Enter b: ";
+    cin >> b;
+    sum = a + b;
+    cout <<"Sum is " << sum ;
+    return 0;
+
+}
