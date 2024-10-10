5d7552988bbd27b0ec0e722e35f70dab15f407e0
Ibrahim-Salman27 <bscs24145@itu.edu.pk>
2024-10-10 09:57:52 +0500

    now program for diff

--- file1.cpp
+++ file1.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main()
 {
     int a , b;
-    int sum = 0;
+    int diff = 0;
     cout<<"Enter a: ";
     cin>>a;
     cout<<"Enter b: ";
     cin >> b;
-    sum = a + b;
-    cout <<"Sum is " << sum ;
+    diff = a - b;
+    cout <<"Sum is " << diff;
     return 0;
 
 }
